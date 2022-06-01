96e535201bf907c0807e2477f017df0548989287
Matheus Duarte <matheusdcarvalho007@gmail.com>
2022-06-01 11:33:56 -0300

    update timestamp in date filter

--- src/modules/bankstatement/controller/BankStatementController.js
+++ src/modules/bankstatement/controller/BankStatementController.js
@@ -4,8 +4,8 @@ const BankStatementController = {
         const initialDate = document.getElementById("initialDate").value;
         const finalDate = document.getElementById("finalDate").value;
         const params = {
-            initialDate: new Date(initialDate).toISOString(),
-            finalDate: new Date(finalDate).toISOString()
+            initialDate: new Date(new Date(initialDate).setUTCHours(0, 0, 0, 0)).toISOString(),
+            finalDate: new Date(new Date(finalDate).setUTCHours(23, 59, 59, 999)).toISOString()
         }
         validateBankStatement(params)
         const response = await BankStatementModel.BankStatement(params)
